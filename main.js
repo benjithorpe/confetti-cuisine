5775d7a790f416fd9323e1d48d07a24df1dc8f81
benjithorpe <benthorpe.dev@gmail.com>
2022-08-14 18:15:01 +0000

    add custom error handlers

--- main.js
+++ main.js
@@ -5,13 +5,14 @@ const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 
 const homeController = require('./controllers/homeController.js');
+const errorController = require('./controllers/errorController.js');
 
-// Variables
+// Config Variables
 const app = express();
-
-// Middlewares
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
+
+// Middlewares
 app.use(ejsLayouts);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,6 +23,9 @@ app.get('/', (req, res) => res.render('index'));
 app.get('/courses.html', homeController.showCourses);
 app.get('/contact.html', homeController.showSignup);
 app.post('/contact', homeController.showPostedSignUpForm);
+
+app.use(errorController.pageNotFoundError);
+app.use(errorController.internalServerError);
 
 app.listen(app.get('port'), () =>
   console.log(`Started on http://localhost:${app.get('port')}`),
